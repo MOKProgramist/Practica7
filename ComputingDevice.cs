4dfab66418c413b79e6d713560f336dc6d85ca63
315 <315@315-14>
2022-12-05 14:59:07 +0300

    Точка входа для создания компьютера

--- ComputingDevice.cs
+++ ComputingDevice.cs
@@ -6,22 +6,42 @@ using System.Threading.Tasks;
 
 namespace Practica7
 {
+    enum TypeDevice
+    {
+        PHONE, PAD, PK
+    }
     internal class ComputingDevice
     {
+ 
         public string Name { get; set; }
-        public string Type { get; set; }
-        public DateOnly YearOfIssue { get; set; }
+        public TypeDevice Type { get; set; }
+        public DateTime YearOfIssue { get; set; }
 
-        public ComputingDevice(string name, string type, DateOnly yearOfIssue)
+        private Computer Computer { get; set; }
+        public ComputingDevice(string name, TypeDevice type, DateTime yearOfIssue)
         {
             Name = name;
             Type = type;
             YearOfIssue = yearOfIssue;
+
+            if (type == TypeDevice.PK)
+            {
+                // Создаем компьютер
+                Display display = new Display(brand: "ЛОС", screenSize: "27", yearOfIssue: DateTime.Now);
+                Processor processor = new Processor(brand: "AMD", power: "4.2гц");
+                Winchester winchester = new Winchester(size: "1 террабайт", type: "3.5");
+
+                Computer computer = new Computer(display, processor, winchester);
+
+                Computer = computer;
+            }
         }
 
         public virtual void Print()
         {
             Console.WriteLine($"Вычислительное устройство с названием {Name} имеет тип {Type} и годом выпуска {YearOfIssue}");
+            Computer.Print();
         }
+
     }
 }
