92d8229e4f67211d0181ccf0ee8d880778b5e3f9
315 <315@315-14>
2022-12-05 15:09:14 +0300

    Сборка человека

--- Program.cs
+++ Program.cs
@@ -4,4 +4,11 @@ using Practica7;
 Console.WriteLine("Начинаем создавать компьютер:");
 // собираем компьютер
 ComputingDevice computingDevice = new ComputingDevice(name: "Рабочая машинка", type: TypeDevice.PK, yearOfIssue: DateTime.Now);
-computingDevice.Print();
+// computingDevice.Print();
+
+// пользователь
+Users user = new Users(lastName: "Андрей", patronymic: "Романович", firstName: "Романов",
+    typeUsers: TypeUsers.Simple, address: "Москва", computer: computingDevice.Divice,
+    dateOfBirth: DateTime.Now
+    );
+user.Print();
