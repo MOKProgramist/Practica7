d622b7945d76ff1e05995921f8155a6349e160b0
315 <315@315-14>
2022-12-05 14:41:19 +0300

    Вывод информации о комплектующих

--- Computer.cs
+++ Computer.cs
@@ -8,9 +8,23 @@ namespace Practica7
 {
     internal class Computer
     {
+        public Display Display { get; set; }
+        public Processor Processor { get; set; }
+        public Winchester Winchester { get; set; }
+
+        public Computer(Display display, Processor processor, Winchester winchester)
+        {
+            Display = display;
+            Processor = processor;
+            Winchester = winchester;
+        }
+
         public virtual void Print()
         {
-            Console.WriteLine("Вывод информации о чем-то");
+            Console.WriteLine("Ввывод информации о компьютере: ");
+            Display.Print();
+            Processor.Print();
+            Winchester.Print();
         }
     }
 }
