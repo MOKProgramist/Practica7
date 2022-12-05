b14f0c54cbc6fff7a4fba069921cc2595420f8d1
315 <315@315-14>
2022-12-05 14:22:52 +0300

    Описали дисплей

--- Display.cs
+++ Display.cs
@@ -9,5 +9,20 @@ namespace Practica7
 {
     internal class Display
     {
+        public string Brand { get; set; }
+        public string ScreenSize { get; set; }
+        public DateOnly YearOfIssue { get; set; }
+
+        public Display(string brand, string screenSize, DateOnly yearOfIssue)
+        {
+            Brand = brand;
+            ScreenSize = screenSize;
+            YearOfIssue = yearOfIssue;
+        }
+
+        public virtual void Print()
+        {
+            Console.WriteLine($"Дисплей от бренда {Brand} имеет размер экрана {ScreenSize} и годом выпуска {YearOfIssue}");
+        }
     }
 }
