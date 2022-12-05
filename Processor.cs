53d0d1cec01a8ce4cfd62789f836550f20ed7f86
315 <315@315-14>
2022-12-05 14:21:11 +0300

    Добавлена сущность процессора

--- Processor.cs
+++ Processor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,24 @@ namespace Practica7
 {
     internal class Processor
     {
+
+        public string Brand { get; set; }
+        // мощность цп
+        public string Power { get; set; }
+
+        // наличие вентилятора
+        public bool IsFan {get; set;}
+
+        public Processor(string brand, string power, bool isFan) 
+        {
+           Brand = brand;
+            Power = power;
+            IsFan = isFan;
+        }
+
+        public virtual void Print()
+        {
+            Console.WriteLine($"Процессор от бренда {Brand} с мощностью {Power} {(IsFan ? "не имеет вентилятор" : "с вентилятором")}");
+        }
     }
 }
