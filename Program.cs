40b8296cca1661b67952f7829f60edd4353042fe
315 <315@315-14>
2022-12-05 14:59:16 +0300

    Изменили точку входа

--- Program.cs
+++ Program.cs
@@ -2,10 +2,6 @@
 using Practica7;
 
 Console.WriteLine("Начинаем создавать компьютер:");
-Display display = new Display(brand: "ЛОС", screenSize: "27", yearOfIssue: DateTime.Now);
-Processor processor = new Processor(brand: "AMD", power: "4.2гц");
-Winchester winchester = new Winchester(size: "1 террабайт", type: "3.5");
-// присоединеения комплектующих 
-Computer computer = new Computer(display, processor, winchester);
-// информвация о компьютере
-computer.Print();
+// собираем компьютер
+ComputingDevice computingDevice = new ComputingDevice(name: "Рабочая машинка", type: TypeDevice.PK, yearOfIssue: DateTime.Now);
+computingDevice.Print();
