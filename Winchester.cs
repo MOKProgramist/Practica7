26ce9cc02736fd64454920554cee62aa705833a6
315 <315@315-14>
2022-12-05 14:23:21 +0300

    Описали винчестер

--- Winchester.cs
+++ Winchester.cs
@@ -8,5 +8,18 @@ namespace Practica7
 {
     internal class Winchester
     {
+        public string Size { get; set; }
+        public string Type { get; set; }
+
+        public Winchester(string size, string type)
+        {
+            Size = size;
+            Type = type;
+        }
+
+        public virtual void Print()
+        {
+            Console.WriteLine($"Винчестер имеет размер {Size} и тип {Type}");
+        }
     }
 }
